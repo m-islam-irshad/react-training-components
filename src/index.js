3af5afc64035208b50faa535282bd91cf35d1a76
mi-ibne-irshad <islambinirshad@gmail.com>
2022-09-06 11:19:02 +0500

    component and props is ready

--- src/index.js
+++ src/index.js
@@ -1,25 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { faker } from '@faker-js/faker';
-
+import CommentDetail from "./components/CommentDetail";
+import ApprovalCard from "./components/ApprovalCard";
 
 const App = ()=>{
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()} />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Muhammad Islam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
+            <ApprovalCard/>
+            <CommentDetail 
+                author="Muhammad Islam" 
+                timeAgo="Today at 4:45PM" 
+                image={faker.image.avatar()} 
+                content="Nice blog post"
+
+            />
+            <CommentDetail 
+                author="Muhammad Muzammal" 
+                timeAgo="Today at 2:00PM" 
+                image={faker.image.avatar()} 
+                content="I like the subject"
+
+            />
+            <CommentDetail 
+                author="Muhammad Hamza" 
+                timeAgo="Today at 5:00PM" 
+                image={faker.image.avatar()} 
+                content="I like the writing"
+
+            />
+            <CommentDetail 
+                author="Muhammad Talha" 
+                timeAgo="Today at 9:00AM" 
+                image={faker.image.avatar()} 
+                content="I like the topic"
+
+            />
         </div>
     )
 }; 
